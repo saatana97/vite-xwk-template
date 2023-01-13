96c6f39030666f2ffd6ccb0f53a22faaea0ff543
向文可 <1041367524@qq.com>
2023-01-13 14:37:59 +0800

    feat: plugins

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import plugin from './plugins';
 import router from './router';
 import pinia from './stores';
 
-import '@/styles/main.less';
-
+import './styles/main.less';
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(plugin);
 
 app.mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,7 @@
-<template>HOME</template>
-<script setup lang="ts"></script>
+<template>
+    <img height="512" :src="logo" width="512" />
+</template>
+<script setup lang="ts">
+    import logo from '~/logo.svg';
+</script>
 <style scoped lang="less"></style>
